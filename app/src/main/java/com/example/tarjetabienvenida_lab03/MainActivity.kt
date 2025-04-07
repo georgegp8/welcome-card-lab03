044680c85ace9e309f1d297800ce1ab920bb7b2e
George Guerra <george.guerra@tecsup.edu.pe>
2025-04-07 01:11:31 -0500

    Creación de una appa de tarjetas de presentación

---
+++ app/src/main/java/com/example/tarjetabienvenida_lab03/MainActivity.kt
@@ -0,0 +1,133 @@
+package com.example.tarjetabienvenida_lab03
+
+import androidx.compose.material3.Surface
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tarjetabienvenida_lab03.ui.theme.TarjetaBienvenidalab03Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TarjetaBienvenidalab03Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color(0xFFDDE6DC)
+                ) {
+                    BusinessCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BusinessCard() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFFDDE6DC)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            PerfilSection()
+            ContactSection()
+        }
+    }
+}
+
+
+@Composable
+fun PerfilSection() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.android_logo),
+            contentDescription = null,
+            modifier = Modifier
+                .size(100.dp)
+                .background(color = Color(0xFF0B5345))
+
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "Jennifer Doe",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Android Developer Extraordinaire",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Medium,
+            color = Color(0xFF0B5345)
+        )
+    }
+}
+
+
+@Composable
+fun ContactSection() {
+    Column(
+        verticalArrangement = Arrangement.Center
+    ) {
+        ContactRow(icon = Icons.Default.Phone, text = "+11 (123) 444 555 666")
+        ContactRow(icon = Icons.Default.Share, text = "@AndroidDev")
+        ContactRow(icon = Icons.Default.Email, text = "jen.doe@android.com")
+    }
+}
+
+
+@Composable
+fun ContactRow(icon: ImageVector, text: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(vertical = 8.dp)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = Color(0xFF0B5345),
+            modifier = Modifier.padding(end = 16.dp)
+        )
+        Text(text = text)
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun BusinessCardPreview() {
+    BusinessCard()
+}
